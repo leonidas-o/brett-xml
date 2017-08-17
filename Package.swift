0376855bb132be2e512aae0161f259b32a57a642
Leonid Orsulic <lorsulic@Leonids-MBP16.fritz.box>
2017-08-17 19:03:32 +0200

    adds products .library

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "BML",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "BML",
+            targets: ["BML"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "2.1.2")),
         .package(url: "https://github.com/vapor/node.git", .upToNextMajor(from: "2.1.0"))
