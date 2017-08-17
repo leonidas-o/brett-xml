0e95d0871df8188dcf8bdddf18a9c032e5eec706
Leonid Orsulic <lorsulic@Leonids-MBP16.fritz.box>
2017-08-17 14:05:50 +0200

    adds targets

--- Package.swift
+++ Package.swift
@@ -6,5 +6,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "2.1.2")),
         .package(url: "https://github.com/vapor/node.git", .upToNextMajor(from: "2.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "BML",
+            dependencies: ["Core", "Node"]),
+        .testTarget(
+            name: "BMLTests",
+            dependencies: ["BML"]),
     ]
 )
